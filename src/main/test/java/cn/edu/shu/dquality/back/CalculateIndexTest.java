7aad5d2fd1b8fc027646327f3578bfde95cc1810
lipanpan03 <656461146@qq.com>
2019-11-11 14:02:16 +0800

    'lpp_local'

---
+++ src/main/test/java/cn/edu/shu/dquality/back/CalculateIndexTest.java
@@ -0,0 +1,13 @@
+package cn.edu.shu.dquality.back;
+
+import org.apache.spark.sql.Dataset;
+import org.junit.Test;
+
+public class CalculateIndexTest {
+
+    @Test
+    public void testCalculateIndex(){
+        Dataset
+    }
+
+}
